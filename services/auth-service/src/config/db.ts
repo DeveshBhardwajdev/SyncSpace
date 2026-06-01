ddb34022c4139d4dc5139b46cebd668777798b89
Devesh Bhardwaj <devdoescoding@gmail.com>
2026-06-02 04:16:02 +0530

    fix(auth-service): fix db.ts MONGODB_URI and clean up .env formatting

--- services/auth-service/src/config/db.ts
+++ services/auth-service/src/config/db.ts
@@ -1,29 +1,26 @@
-import { error } from "console";
 import mongoose from "mongoose";
 
+export const connectDB = async(): Promise<void> => {
+    try {
+        const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectDB = async(): Promise<void> =>{
-    try{
-
-        const mongoDB_URI = process.env.MONGO_URI;
-
-        if(!mongoDB_URI){
-            throw new Error("MONGO_URI is not defined in this environment variables. Check your .env file");
+        if(!MONGODB_URI){
+            throw new Error("MONGODB_URI is not defined in environment variables. Check your .env file");
         }
 
-        const connection = await mongoose.connect(mongoDB_URI);
+        const connection = await mongoose.connect(MONGODB_URI);
+        console.log(`MongoDB connected: ${connection.connection.host}`);
 
-        console.log(`MongoDB connected:${connection.connection.host}`);
-    }catch(error){
-        console.error("MongoDB connection failed:",error);
+    } catch(error) {
+        console.error("MongoDB connection failed:", error);
         process.exit(1);
     }
 };
 
-mongoose.connection.on('disconnected',() =>{
+mongoose.connection.on('disconnected', () => {
     console.warn("MongoDB disconnected");
 });
 
-mongoose.connection.on('error',()=>{
-    console.log("MongoDB connection Error",error)
-})
+mongoose.connection.on('error', (err) => {
+    console.error("MongoDB connection error:", err);
+});
