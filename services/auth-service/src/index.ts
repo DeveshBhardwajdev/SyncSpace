7704ba63699c8e9472f6923c3e6fa4cc105572b4
Devesh Bhardwaj <devdoescoding@gmail.com>
2026-06-06 14:57:14 +0530

    day5: add cookie-parser middleware to index.ts

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -2,6 +2,7 @@ import express, { Application,Request,Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -22,6 +23,7 @@ app.use(morgan('dev'));
 app.use(express.json())
 
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser()); 
 
 app.get('/health',(req:Request, res:Response)=>{
     res.status(200).json({
